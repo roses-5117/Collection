f3f0ed1a43d24efcd74a6228097ba1ecf1243fd1
NoeShinohara <163381729+roses-5117@users.noreply.github.com>
2025-07-01 21:54:58 +0900

    課題提出

--- src/practice/Task.java
+++ src/practice/Task.java
@@ -1,5 +1,18 @@
 package practice;
 
+import java.time.LocalDate;
+
 public class Task {
-  //プログラムを作成
+  LocalDate date;
+  String content;
+
+  public Task(LocalDate date, String content) {
+    this.date = date;
+    this.content = content;
+  }
+
+  @Override
+  public String toString() {
+    return date + " " + content;
+  }
 }
